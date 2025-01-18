1cdd84801ea71b5b0400f33be46bf136e046f8ad
Brunna <brunnabarreto363@gmail.com>
2025-01-18 20:57:56 -0300

    Add tecla#.c and tecla0.c

---
+++ tecla0.c
@@ -0,0 +1,21 @@
+// tecla_0
+
+#include "tecla_0.h"
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+void handle_key_0(uint gpio_led) {
+    gpio_put(gpio_led, 1);
+    sleep_ms(500);
+    gpio_put(gpio_led, 0);
+}
+
+// tecla_0.h
+#ifndef TECLA_0_H
+#define TECLA_0_H
+
+#include <stdint.h>
+
+void handle_key_0(uint gpio_led);
+
+#endif
